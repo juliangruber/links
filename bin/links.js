f31871b270745fcd1f69753ab6b4f21ec0717c85
Julian Gruber <julian@juliangruber.com>
2013-12-10 17:29:37 +0100

    add footer cli arg

--- bin/links.js
+++ bin/links.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node --harmony-generators
 
-var app = require('..');
+var Links = require('..');
 var minimist = require('minimist');
 
 var argv = minimist(process.argv.slice(2));
 var port = argv.port || argv.p || 3000;
+var app = Links({ footer: argv.footer });
 
 app.listen(port);
 console.log('open http://localhost:%s/', port);
--- index.js
+++ index.js
@@ -11,51 +11,64 @@ var logger = require('koa-logger');
 var uid = require('uid2');
 var parse = require('co-body');
 
-var db = level('db', { db: leveldown });
-var app = module.exports = koa();
-
-app.use(logger());
-app.use(serve(__dirname + '/public'));
-
-app.use(get(/^\/([A-z0-9]{32})?$/, show));
-app.use(put(/^\/([A-z0-9]{32})?$/, update));
-app.use(post('/fork', fork));
-
 /**
- * Show content.
+ * Create a new links app.
+ *
+ * @param {Object=} opts
+ * @return {Koa}
  */
 
-function* show(id) {
-  var content;
-  if (id) content = yield db.get('content:' + id);
-  this.body = yield render(__dirname + '/index.jade', {
-    id: id,
-    content: content
-  });
-}
+module.exports = function(opts) {
+  if (!opts) opts = {};
+  var db = level('db', { db: leveldown });
+  var app = koa();
+  
+  app.use(logger());
+  app.use(serve(__dirname + '/public'));
+  
+  app.use(get(/^\/([A-z0-9]{32})?$/, show));
+  app.use(put(/^\/([A-z0-9]{32})?$/, update));
+  app.use(post('/fork', fork));
+  
+  /**
+   * Show content.
+   */
+  
+  function* show(id) {
+    var content;
+    if (id) content = yield db.get('content:' + id);
+    this.body = yield render(__dirname + '/index.jade', {
+      id: id,
+      content: content,
+      footer: opts.footer
+    });
+  }
+  
+  /**
+   * Fork content and generate a token.
+   */
+  
+  function* fork() {
+    var token = uid(32);
+    var forkId = uid(32);
+    yield db.put('token:' + forkId, token);
+    this.body = {
+      token: token,
+      id: forkId
+    };
+  }
+  
+  /**
+   * Update content if tokens match.
+   */
+  
+  function* update(id) {
+    var body = yield parse.json(this);
+    var token = yield db.get('token:' + id);
+    if (token != body.token) return this.throw(403);
+    yield db.put('content:' + id, body.content);
+    this.body = 'ok';
+  }
 
-/**
- * Fork content and generate a token.
- */
-
-function* fork() {
-  var token = uid(32);
-  var forkId = uid(32);
-  yield db.put('token:' + forkId, token);
-  this.body = {
-    token: token,
-    id: forkId
-  };
-}
-
-/**
- * Update content if tokens match.
- */
-
-function* update(id) {
-  var body = yield parse.json(this);
-  var token = yield db.get('token:' + id);
-  if (token != body.token) return this.throw(403);
-  yield db.put('content:' + id, body.content);
-  this.body = 'ok';
+  return app;
 }
